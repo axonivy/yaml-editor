c34564a080641ecc9a39e34e0e9a208eca594c4d
Louis <louis.mueller@axonivy.com>
2023-07-14 16:22:45 +0200

    push next

--- integrations/standalone/src/index.tsx
+++ integrations/standalone/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 import { YAMLVariablesTable } from '@axonivy/config-editor';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-cosnt XMLDocument;
+
 root.render(
     <React.StrictMode>
         <YAMLVariablesTable />
