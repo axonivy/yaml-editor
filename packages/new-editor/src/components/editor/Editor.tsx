f10be963860ccf01a4d67b5834194b85c59e3801
Lukas Giger <lukas.giger@axonivy.com>
2024-03-27 11:05:31 +0100

    XIVY-11980 implement Variables component

--- packages/new-editor/src/components/editor/Editor.tsx
+++ packages/new-editor/src/components/editor/Editor.tsx
@@ -1,16 +1,23 @@
 import { Flex, ResizableHandle, ResizablePanel, ResizablePanelGroup, SidebarHeader, Toolbar, ToolbarTitle } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import './Editor.css';
+import { Variables } from './variables/master/Variables';
 
 export const Editor = () => {
   // TODO: Variables coming from context
   const masterTitle = 'Rest Clients Editor';
-  const masterContent = (
-    <Flex justifyContent='center' alignItems='center' style={{ height: '100%' }}>
-      <span>Content</span>
-    </Flex>
-  );
   const detailTitle = 'Variables Editor - variableA';
+  const variables = [
+    {
+      key: 'param.procurementRequest',
+      value: '',
+      children: [
+        { key: 'accepted', children: [], value: 'true' },
+        { key: 'amount', children: [], value: '1234' },
+        { key: 'requester', value: '', children: [{ key: 'user', value: 'Admin', children: [] }] }
+      ]
+    }
+  ];
   const detailContent = (
     <Flex justifyContent='center' alignItems='center' style={{ height: '100%' }}>
       <span>Properties</span>
@@ -18,18 +25,20 @@ export const Editor = () => {
   );
 
   return (
-    <ResizablePanelGroup direction='horizontal' style={{ minHeight: 200 }}>
+    <ResizablePanelGroup direction='horizontal' className='editor'>
       <ResizablePanel defaultSize={75} minSize={50} className='master-panel'>
-        <Flex direction='column' style={{ height: '100%' }}>
+        <Flex direction='column'>
           <Toolbar className='master-toolbar'>
             <ToolbarTitle>{masterTitle}</ToolbarTitle>
           </Toolbar>
-          {masterContent}
+          <Flex className='master-content'>
+            <Variables variables={variables} />
+          </Flex>
         </Flex>
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel defaultSize={25} minSize={10}>
-        <Flex direction='column' style={{ height: '100%' }}>
+        <Flex direction='column'>
           <SidebarHeader icon={IvyIcons.PenEdit} title={detailTitle} />
           {detailContent}
         </Flex>
