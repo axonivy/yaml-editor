d868f89111bb6c284f20864499a3027822d03c0c
Reto Weiss <reto.weiss@axonivy.com>
2024-06-12 14:51:56 +0200

    XIVY-14000 Add playwright tests to build

--- integrations/standalone/tests/mock/variable-editor.spec.ts
+++ integrations/standalone/tests/mock/variable-editor.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle('Variables Editor');
+  await page.goto('/mock.html');
+  await expect(page).toHaveTitle('Variables Editor Mock');
 });
 
 test('search', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/mock.html');
   const search = page.locator('.ui-input');
   await search.fill('Hello');
   await expect(search).toHaveValue('Hello');
