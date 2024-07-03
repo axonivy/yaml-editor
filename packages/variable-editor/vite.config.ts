2a7f85615b8ee8961ef1b8b72d0415779d29525c
Louis <louis.mueller@axonivy.com>
2024-07-03 14:26:18 +0200

    XIVY-14104 vite editor to make it esm conform

---
+++ packages/variable-editor/vite.config.ts
@@ -0,0 +1,32 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [react(), dts({ tsconfigPath: './tsconfig.production.json' })],
+  esbuild: {
+    sourcemap: 'inline'
+  },
+  build: {
+    outDir: 'lib',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'editor',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: [
+        '@axonivy/ui-components',
+        '@axonivy/ui-icons',
+        '@tanstack/react-query',
+        '@tanstack/react-query-devtools',
+        'react',
+        'react-error-boundary',
+        'react/jsx-runtime',
+        'react-dom',
+        'yaml'
+      ]
+    }
+  }
+});
