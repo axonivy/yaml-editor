03f4412f3f801c0f92d06d228d47902ca07291a7
Alexander Suter <alexander.suter@axonivy.com>
2024-07-19 08:39:53 +0200

    Fix .ivy automation scripts after refactoring repo-structure

--- .ivy/raise-build-plugin-version.sh
+++ .ivy/raise-build-plugin-version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
-mvn --batch-mode versions:set-property versions:commit -f integrations/standalone/tests/integration/project/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
+mvn --batch-mode versions:set-property versions:commit -f integrations/standalone/tests/integration/projects/connector/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
+mvn --batch-mode versions:set-property versions:commit -f integrations/standalone/tests/integration/projects/variables/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
+
