dc2cfa07b3a1639cddc8170bd5fd51d1c73d5d06
Lukas Giger <lukas.giger@axonivy.com>
2024-08-30 13:12:06 +0200

    refactor: remove barrel files

--- packages/variable-editor/src/index.ts
+++ packages/variable-editor/src/index.ts
@@ -1,3 +1,5 @@
+export * from './protocol/client-json-rpc';
+export * from './protocol/ClientContextProvider';
+export * from './query/query-client';
+export * from './query/QueryProvider';
 export { default as VariableEditor } from './VariableEditor';
-export * from './protocol';
-export * from './query';
