b4282f3dfd9941f4c8e01f6adf5a4c195504cd27
Lukas Lieb <lukas.lieb@axonivy.com>
2024-11-04 11:50:02 +0100

    Move playwright to own package

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-mvn --batch-mode -f integrations/standalone/tests/screenshots/pom.xml versions:set versions:commit -DnewVersion=${1}
 mvn --batch-mode -f integrations/standalone/pom.xml versions:set versions:commit -DnewVersion=${1}
-mvn --batch-mode -f integrations/standalone/tests/integration/projects/connector/pom.xml versions:set versions:commit -DnewVersion=${1}
-mvn --batch-mode -f integrations/standalone/tests/integration/projects/variables-test-project/pom.xml versions:set versions:commit -DnewVersion=${1}
+mvn --batch-mode -f playwright/tests/screenshots/pom.xml versions:set versions:commit -DnewVersion=${1}
+mvn --batch-mode -f playwright/projects/connector/pom.xml versions:set versions:commit -DnewVersion=${1}
+mvn --batch-mode -f playwright/projects/variables-test-project/pom.xml versions:set versions:commit -DnewVersion=${1}
 
 npm install
 yarn lerna version ${1/-SNAPSHOT/-next} --no-git-tag-version --no-push --ignore-scripts --exact --yes
