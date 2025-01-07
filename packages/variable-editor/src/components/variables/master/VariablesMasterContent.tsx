e951c5ac9fd0e39f09415eb33ba720701bd7747f
ivy-edp <elio.dipuma@axonivy.com>
2025-01-07 12:27:08 +0100

    XIVY-13545 Add Navigation via ArrowKey in Main-Table

--- packages/variable-editor/src/components/variables/master/VariablesMasterContent.tsx
+++ packages/variable-editor/src/components/variables/master/VariablesMasterContent.tsx
@@ -11,12 +11,13 @@ import {
   TableResizableHeader,
   useReadonly,
   useTableExpand,
+  useTableKeyHandler,
   useTableSelect
 } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { getCoreRowModel, useReactTable, type ColumnDef } from '@tanstack/react-table';
 import { useAppContext } from '../../../context/AppContext';
-import { deleteFirstSelectedRow, useTreeGlobalFilter } from '../../../utils/tree/tree';
+import { deleteFirstSelectedRow, toTreePath, useTreeGlobalFilter } from '../../../utils/tree/tree';
 import { type Variable } from '../data/variable';
 import { variableIcon } from '../data/variable-utils';
 import { AddVariableDialog } from '../dialog/AddDialog';
@@ -25,9 +26,15 @@ import { ValidationRow } from './ValidationRow';
 import './VariablesMasterContent.css';
 
 export const VariablesMasterContent = () => {
-  const { variables, setVariables, setSelectedVariable } = useAppContext();
+  const { variables, setVariables, setSelectedVariable, detail, setDetail } = useAppContext();
 
-  const selection = useTableSelect<Variable>();
+  const selection = useTableSelect<Variable>({
+    onSelect: selectedRows => {
+      const selectedRowId = Object.keys(selectedRows).find(key => selectedRows[key]);
+      const selectedVariable = table.getRowModel().flatRows.find(row => row.id === selectedRowId)?.id;
+      setSelectedVariable(toTreePath(selectedVariable));
+    }
+  });
   const expanded = useTableExpand<Variable>();
   const globalFilter = useTreeGlobalFilter(variables);
   const columns: Array<ColumnDef<Variable, string>> = [
@@ -57,16 +64,19 @@ export const VariablesMasterContent = () => {
     }
   });
 
+  const { handleKeyDown } = useTableKeyHandler({
+    table,
+    data: variables
+  });
+
   const deleteVariable = () =>
     setVariables(old => {
       const deleteFirstSelectedRowReturnValue = deleteFirstSelectedRow(table, old);
-      setSelectedVariable(deleteFirstSelectedRowReturnValue.selectedPath);
       return deleteFirstSelectedRowReturnValue.newData;
     });
 
   const resetSelection = () => {
     selectRow(table);
-    setSelectedVariable([]);
   };
 
   const readonly = useReadonly();
@@ -89,7 +99,7 @@ export const VariablesMasterContent = () => {
     <Flex direction='column' className='master-content-container' onClick={resetSelection}>
       <BasicField className='master-content' label='List of variables' control={control} onClick={event => event.stopPropagation()}>
         {globalFilter.filter}
-        <Table>
+        <Table onKeyDown={e => handleKeyDown(e, () => setDetail(!detail))}>
           <TableResizableHeader headerGroups={table.getHeaderGroups()} onClick={resetSelection} />
           <TableBody>
             {table.getRowModel().rows.map(row => (
