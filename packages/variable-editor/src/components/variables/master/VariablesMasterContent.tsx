4209754b45f74a34fccfeece978512eda4fd8ca9
Lukas Giger <lukas.giger@axonivy.com>
2025-02-05 08:27:59 +0100

    XIVY-14462 add row virtualization
    - Virtualize rows to improve performance.
    - Hard code row height to `36px`.
    - Set specific values for sizes in the column defintion because the new layout requires it.
    - Fix right side of border on rows with messages not being visible.
    
    Existing problems:
    - Did not manage to make table header sticky.
    - Because of the new layout, the columns are not automatically using all the available space.
      This means that the rightmost column will be cut off even though there seems to be more space available.
      And because of our implementation of `TableResizableHeader`, the rightmost row is currently not resizable.

--- packages/variable-editor/src/components/variables/master/VariablesMasterContent.tsx
+++ packages/variable-editor/src/components/variables/master/VariablesMasterContent.tsx
@@ -21,6 +21,7 @@ import {
 } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { getCoreRowModel, useReactTable, type ColumnDef } from '@tanstack/react-table';
+import { useVirtualizer } from '@tanstack/react-virtual';
 import { useRef } from 'react';
 import { useAppContext } from '../../../context/AppContext';
 import { useKnownHotkeys } from '../../../utils/hotkeys';
@@ -31,6 +32,8 @@ import { AddVariableDialog } from '../dialog/AddDialog';
 import { OverwriteDialog } from '../dialog/OverwriteDialog';
 import { ValidationRow } from './ValidationRow';
 import './VariablesMasterContent.css';
+
+export const ROW_HEIGHT = 36 as const;
 
 export const VariablesMasterContent = () => {
   const { variables, setVariables, setSelectedVariable, detail, setDetail } = useAppContext();
@@ -48,13 +51,22 @@ export const VariablesMasterContent = () => {
     {
       accessorKey: 'name',
       header: header => <ExpandableHeader name='Name' header={header} />,
-      cell: cell => <ExpandableCell cell={cell} icon={variableIcon(cell.row.original)} />,
-      minSize: 50
+      cell: cell => (
+        <ExpandableCell cell={cell} icon={variableIcon(cell.row.original)}>
+          <span>{cell.getValue()}</span>
+        </ExpandableCell>
+      ),
+      minSize: 200,
+      size: 500,
+      maxSize: 1000
     },
     {
       accessorFn: (variable: Variable) => (variable.metadata.type === 'password' ? '***' : variable.value),
       header: 'Value',
-      cell: cell => <div>{cell.getValue()}</div>
+      cell: cell => <span>{cell.getValue()}</span>,
+      minSize: 200,
+      size: 500,
+      maxSize: 1000
     }
   ];
   const table = useReactTable({
@@ -69,6 +81,25 @@ export const VariablesMasterContent = () => {
       ...expanded.tableState,
       ...globalFilter.tableState
     }
+  });
+
+  const rows = table.getRowModel().rows;
+  const tableContainer = useRef<HTMLDivElement>(null);
+  const measureElement = (element: HTMLTableRowElement) => {
+    let height = ROW_HEIGHT;
+    let nextElement = element.nextElementSibling;
+    while (nextElement?.classList.contains('ui-message-row')) {
+      height += ROW_HEIGHT;
+      nextElement = nextElement.nextElementSibling;
+    }
+    return height;
+  };
+  const virtualizer = useVirtualizer<HTMLDivElement, HTMLTableRowElement>({
+    count: rows.length,
+    estimateSize: () => ROW_HEIGHT,
+    measureElement,
+    getScrollElement: () => tableContainer.current,
+    overscan: 20
   });
 
   const { handleKeyDown } = useTableKeyHandler({
@@ -124,14 +155,17 @@ export const VariablesMasterContent = () => {
         onClick={event => event.stopPropagation()}
       >
         {globalFilter.filter}
-        <Table onKeyDown={e => handleKeyDown(e, () => setDetail(!detail))} style={{ overflowX: 'unset' }}>
-          <TableResizableHeader headerGroups={table.getHeaderGroups()} onClick={resetSelection} />
-          <TableBody>
-            {table.getRowModel().rows.map(row => (
-              <ValidationRow key={row.id} row={row} />
-            ))}
-          </TableBody>
-        </Table>
+        <div ref={tableContainer} className='virtual-table-container'>
+          <Table onKeyDown={e => handleKeyDown(e, () => setDetail(!detail))} style={{ display: 'grid' }}>
+            <TableResizableHeader headerGroups={table.getHeaderGroups()} onClick={resetSelection} />
+            <TableBody style={{ height: `${virtualizer.getTotalSize()}px` }} className='virtual-table-body'>
+              {virtualizer.getVirtualItems().map(virtualRow => {
+                const row = rows[virtualRow.index];
+                return <ValidationRow key={row.id} row={row} virtualRow={virtualRow} virtualizer={virtualizer} />;
+              })}
+            </TableBody>
+          </Table>
+        </div>
       </BasicField>
     </Flex>
   );
